c4d29a89dbab7ea1e6773988733faaf3cd804db1
naobaobao123 <1018773709@qq.com>
2020-01-13 20:36:21 +0800

    狗子店

---
+++ src/test/java/com/dogshop/DogshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
